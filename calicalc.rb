870f1854374ff185150b275adb9d11a5f3bb9d62
HussamAnwar <hussam726@live.com>
2022-04-30 01:15:11 -0400

    added basic memory functionality. cleaned up some code.

--- calicalc.rb
+++ calicalc.rb
@@ -1,10 +1,18 @@
 #needed for code
 require_relative "calcmethods.rb"
 require_relative "ifelsedet.rb"
-
+require_relative "memory.rb"
 #Main below.
 
+intro()
+start = gets.chomp.to_s
+
 loop do #do while loop beginning
+
+#array declared below for memory
+mem = []
+m = mem
+
 puts "Enter the first number: "
 firstNum = gets.chomp.to_f #gets first number from user
 
@@ -38,6 +46,10 @@ else
   puts "Invalid operation please try again"
 end
 
+#stores answer in memory
+memory = memory(ans, m)
+
+puts "#{memory}"
 #Asks for the yes or no decision from user (0 is no, 1 is yes)
 print "Enter 0 to exit or 1 to continue: "
 yOrN = gets.to_i
